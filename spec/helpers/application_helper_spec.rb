c2bf65a04f870799aec91a563ce3df8aa0993657
Christopher Giroir <kelsin@valefor.com>
2014-10-12 08:41:14 -0700

    Adding user_image test

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, :type => :helper do
+  describe "#user_image" do
+    it "should generate an image link" do
+      user = instance_double("User", :image => 'image')
+      expect(helper.user_image(user)).to eql("<img alt=\"Image\" class=\"profile-img\" src=\"/images/image\" />")
+    end
+  end
 end
