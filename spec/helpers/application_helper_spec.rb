7f614149d3bb63565364fcfc8e684108cfa7694b
Christopher Giroir <kelsin@valefor.com>
2014-10-12 08:46:02 -0700

    Better user_image test

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper, :type => :helper do
   describe "#user_image" do
     it "should generate an image link" do
-      user = instance_double("User", :image => 'image')
-      expect(helper.user_image(user)).to eql("<img alt=\"Image\" class=\"profile-img\" src=\"/images/image\" />")
+      user = instance_double("User", :image => 'image_url')
+      image = helper.user_image(user)
+      expect(image).to have_selector(:css, "img")
+      expect(image).to match("image_url")
     end
   end
 end
