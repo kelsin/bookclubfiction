5b47a3a0d3843380c698aadb327589da65df2ab1
Christopher Giroir <kelsin@valefor.com>
2014-10-12 17:52:41 -0700

    Adding proper coveralls support, I think

--- .simplecov
+++ .simplecov
@@ -1,3 +1,6 @@
+require 'coveralls'
+
 SimpleCov.start 'rails' do
+  formatter SimpleCov::Formatter::MultiFormatter[SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
   minimum_coverage 0
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -5,7 +5,9 @@
 # files.
 
 require 'cucumber/rails'
+
 require 'simplecov'
+SimpleCov.coverage_dir 'coverage/features'
 
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
