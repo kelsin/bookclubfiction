2692800fbd9b032e232a0af21a33e969cf83f102
Ayashi <ayashi@valefor.com>
2014-10-16 21:34:14 -0700

    styling selections area to make the user experience more clear

--- app/assets/javascripts/bookclubfiction.js
+++ app/assets/javascripts/bookclubfiction.js
@@ -1,3 +1,3 @@
 (function () {
-  angular.module('BookClubFiction', ['ngRoute']);
+  angular.module('BookClubFiction', ['ngRoute', 'ui.bootstrap']);
 })();
--- app/assets/javascripts/controllers/NominationsController.js
+++ app/assets/javascripts/controllers/NominationsController.js
@@ -40,6 +40,7 @@
     });
   }
 
+
   angular
     .module('BookClubFiction')
     .controller('NominationsController', NominationsController);
