4d5be4910db699e238a713bcc09d43f7c2479160
Christopher Giroir <kelsin@valefor.com>
2014-10-18 19:48:29 -0700

    Adding loading bar

--- app/assets/javascripts/bookclubfiction.js
+++ app/assets/javascripts/bookclubfiction.js
@@ -1,3 +1,3 @@
 (function () {
-  angular.module('BookClubFiction', ['ngRoute', 'ui.bootstrap']);
+  angular.module('BookClubFiction', ['ngAnimate', 'angular-loading-bar', 'ngRoute', 'ui.bootstrap']);
 })();
